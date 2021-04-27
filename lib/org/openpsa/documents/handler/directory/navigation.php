186a1d3abc51837cda576d9f778b4530f624af65
flack <flack@contentcontrol-berlin.de>
2021-04-27 16:07:49 +0200

    import

--- lib/org/openpsa/documents/handler/directory/navigation.php
+++ lib/org/openpsa/documents/handler/directory/navigation.php
@@ -48,10 +48,6 @@ class org_openpsa_documents_handler_directory_navigation extends midcom_baseclas
 
     /**
      * Builds an array with a tree structure from a given array of topics
-     *
-     * @param midcom_db_topic[] $topic_array - array which contains the topics for the tree
-     * @param midcom_db_topic $root_topic - root topic of the tree
-     * @param array $tree_array - array which contains the tree
      */
     private function _tree_array_build(array $topic_array, midcom_db_topic $root_topic, array &$tree_array)
     {
@@ -63,7 +59,7 @@ class org_openpsa_documents_handler_directory_navigation extends midcom_baseclas
         }
     }
 
-    public function _handler_navigation(array &$data)
+    public function _handler_navigation()
     {
         $this->root_topic = $this->_topic;
         while ($this->root_topic->up && $this->root_topic->get_parent()->component == $this->_component) {
@@ -77,9 +73,6 @@ class org_openpsa_documents_handler_directory_navigation extends midcom_baseclas
         $this->topic_array = $qb->execute();
     }
 
-    /**
-     * @param array $data The local request data.
-     */
     public function _show_navigation(string $handler_id, array &$data)
     {
         $tree_array = [
